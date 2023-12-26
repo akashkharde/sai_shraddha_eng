ab73491a06a2e31ca7089884cff347dc34c47ebf
akashkharde <akashkharde111@gmail.com>
2023-12-26 21:09:37 +0530

    assets

--- src/Theme/Ui/Container.js
+++ src/Theme/Ui/Container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children , className}) {
   return (
-    <div className={`${className} container_ui`}>
+    <div className={`${className} container_ui mx-auto`}>
         {children}
     </div>
   )
