24d23556eab3277d8e780d8fb1085d1dfe8a28bf
akashkharde <akashkharde111@gmail.com>
2023-12-28 09:39:32 +0530

    company

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Theme/Common/Header';
 import Button from './Theme/Ui/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Theme/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Company from './Theme/Company';
 import Products from './Theme/Products';
 import Purchase from './Theme/Purchase';
@@ -14,23 +14,32 @@ import Blog from './Theme/Blog';
 import ContactUs from './Theme/ContactUs';
 import PageNotFound from './Theme/PageNotFound';
 import Footer from './Theme/Common/Footer';
+import { useEffect } from 'react';
+import Quality from './Theme/SubComponents/Quality';
+import Services from './Theme/SubComponents/Services';
+import WhyUs from './Theme/SubComponents/WhyUs';
+import MissionVission from './Theme/SubComponents/MissionVission';
 
 function App() {
-
+  
   return (
-    <BrowserRouter>
+    <>
       <Header />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/company' element={<Company />} />
-          <Route exact path='/product' element={<Products />} />
-          <Route exact path='/purchase' element={<Purchase />} />
-          <Route exact path='/blog' element={<Blog />} />
-          <Route exact path='/contact' element={<ContactUs />} />
-          <Route exact path='*' element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/company/quality' element={<Quality />} />
+        <Route path='/company/services' element={<Services />} />
+        <Route path='/company/why-us' element={<WhyUs />} />
+        <Route path='/company/mission-vision' element={<MissionVission />} />
+        <Route path='/product' element={<Products />} />
+        <Route path='/purchase' element={<Purchase />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
