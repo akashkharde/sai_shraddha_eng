963c24480d66f9435a4b0cb9bb8358f5e8557f4c
akashkharde <akashkharde111@gmail.com>
2024-01-07 16:54:33 +0530

    7-1

--- src/Theme/ContactUs.js
+++ src/Theme/ContactUs.js
@@ -1,34 +1,64 @@
 import React from 'react'
 import Container from './Ui/Container';
-import { FaPhoneVolume } from "react-icons/fa6";
-import contactUs from '../Theme/Assets/Img/contactBg.jpg'
+import { FaLocationDot } from "react-icons/fa6";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMailUnreadOutline } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 function ContactUs() {
   return (
-    <div className='contactBg'>
-      {/* <div class="glass"
-       >
-        Hello Folks!
-      </div> */}
+    <div className='contactBg my-4'>
       <Container>
-      <div className='displayCenter flex-column '>
-        <h1 className='contactFont'>Let's Get Connected</h1>
-        <h2 className='fs-2 my-4'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </h2></div>
-        <div className='row'>
-            <div className='col'>
-              <div>
-                <div><FaPhoneVolume /></div>
-                <div>This is Dummy Text</div>
-                <div>+91 7719832542</div>
+        <div className='displayCenter flex-column '>
+          <h1 className='contactFont'>Let's Get Connected</h1>
+          <h2 className='fs-2 my-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h2>
+        </div>
+
+        <div className='row contactRow'>
+          <div className='col-lg-5 col-md-7  col-sm-12 contactCol1 '>
+            <div className='p-4' >
+              <h2 className='heading displayCenter text-white'>This Is heading</h2>
+              <div className=' text text-white'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+              <div className='displayCenter justify-content-start gap-4 mt-4'>
+                <div><FaLocationDot className="text-light fs-3" /></div>
+                <div className='text text-white'>
+                  Mahavir Residency, 624, Spine Rd, Shivatej Nagar, Chinchwad, Pimpri-Chinchwad, Maharashtra 411019
+                </div>
+              </div>
+              <div className='displayCenter justify-content-start gap-4 mt-2'>
+                <div><FaPhoneAlt className="text-light fs-3" /></div>
+                <div className='text text-white'>
+                  +91 7719832542
+                </div>
+              </div>
+              <div className='displayCenter justify-content-start gap-4 mt-2'>
+                <div><IoMailUnreadOutline className="text-light fs-3" /></div>
+                <div className='text text-white'>
+                  itsolution@devitglobe.com
+                </div>
+              </div>
+              <div className='displayCenter justify-content-between  gap-4 mt-2 contactDesignMain'>
+                <div className='displayCenter gap-4 '>
+                  <div><FaFacebook className="text-light fs-3" /></div>
+                  <div><FaInstagram className="text-light fs-3" /></div>
+                  <div><FaLinkedin className="text-light fs-3" /></div>
+                </div>
+                <div className='contactDesign'>
+              <div className="contactDesignsmallcircle"></div>
+
+                </div>
               </div>
             </div>
-            <div className='col'>hii</div>
-            <div className='col'>hii</div>
-
+          </div>
+          <div className='col-lg-7 col-md-7 col-sm-12'>
+           hiii
+            </div>
         </div>
-    </Container>
+      </Container>
     </div>
 
   )
--- src/Theme/SubComponents/WhyUs.js
+++ src/Theme/SubComponents/WhyUs.js
@@ -17,35 +17,35 @@ function WhyUs() {
           <img src={whyChooseUs} alt=' ' className='w-100 imgabout' />
         </div>
         <div className='col-lg-7  col-md-6 col-sm-12 displayCenter flex-column'>
-          <div className='d-flex align-item-center justify-content-evenly w-100 mb-3 '>
+          <div className='d-flex align-item-center justify-content-start w-100 mb-3 '>
             <div className='mx-3 displayCenter'> <img src={increase} alt=' ' className='bg-blue imgIncrease displayCenter' /></div>
             <div className=''>
               <div className='subHeading'>Qalert Innovation </div>
               <div className='text'>Real-time quality monitoring for unparalleled precision.</div>
             </div>
           </div>
-          <div className='d-flex align-item-center justify-content-evenly w-100 mb-3'>
+          <div className='d-flex align-item-center justify-content-start w-100 mb-3'>
             <div className='mx-3 displayCenter'> <img src={increase} alt=' ' className='bg-blue imgIncrease displayCenter' /></div>
             <div className=''>
               <div className='subHeading'>Tailored Solutions:</div>
               <div className='text'>Customized for your industry's unique needs.</div>
             </div>
             </div>
-            <div className='d-flex align-item-center justify-content-evenly w-100 mb-3'>
+            <div className='d-flex align-item-center justify-content-start w-100 mb-3'>
             <div className='mx-3 displayCenter'> <img src={increase} alt=' ' className='bg-blue imgIncrease displayCenter' /></div>
             <div className=''>
               <div className='subHeading'> Proven Excellence:</div>
               <div className='text'>Over 10+ years of unmatched quality and reliability.</div>
             </div>
           </div>
-          <div className='d-flex align-item-center justify-content-evenly w-100 mb-3'>
+          <div className='d-flex align-item-center justify-content-start w-100 mb-3'>
             <div className='mx-3 displayCenter'> <img src={increase} alt=' ' className='bg-blue imgIncrease displayCenter' /></div>
             <div className=''>
               <div className='subHeading'>Sustainability Focus: </div>
               <div className='text'>Minimizing environmental impact through responsible practices.</div>
             </div>
           </div>
-          <div className='d-flex align-item-center justify-content-evenly w-100 mb-3'>
+          <div className='d-flex align-item-center justify-content-start w-100 mb-3'>
             <div className='mx-3 displayCenter'> <img src={increase} alt=' ' className='bg-blue imgIncrease displayCenter' /></div>
             <div className=''>
               <div className='subHeading'>Passionate Expert Team: </div>
